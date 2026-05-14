464c1f39a66dde31954e4d944eea370bfe648a8c
LTD-SN <distribuidora1026@gmail.com>
2026-05-14 15:36:35 -0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyC30HIaWu4XdSeXXaApn9aaqxncG0ODHqo",
+  authDomain: "stock-distribuidora-nuevo.firebaseapp.com",
+  projectId: "stock-distribuidora-nuevo",
+  storageBucket: "stock-distribuidora-nuevo.firebasestorage.app",
+  messagingSenderId: "102295335037",
+  appId: "1:102295335037:web:d7c86531c0818e0eec4114"
+};
